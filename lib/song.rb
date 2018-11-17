3f25f8fd2604835bfca16e1c2aa729ee52679ab9
Katie Ashlin <akka726@outlook.com>
2018-11-17 00:57:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,42 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    song_name = self.new
+    @@all << song_name
+    song_name
+  end
+  
+  def self.new_by_name(song_name)
+    song_object = self.new
+    song_object.name = song_name
+    song_object
+  end
+  
+  def self.create_by_name(song_name)
+    song_object = self.new_by_name(song_name)
+    @@all << song_object
+    song_object
+  end
+  
+  def self.find_by_name(song_name)
+    @@all.find{ |song| song.name == song_name} 
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{ |song| song.name}
+  end
+
+  def self.new_from_filename
+    
+  end
 
   def self.all
     @@all
